c371ab05ce09c202b868847e12342c62b71c1118
neoedmund <neoedmund@380faea4-4b26-11de-8d14-a1ddb34ee23c>
2009-05-28 08:00:53 +0000

    new file and select page

--- src/neoe/ne/PageInfo.java
+++ src/neoe/ne/PageInfo.java
@@ -1,13 +1,27 @@
 package neoe.ne;
 
+import java.io.File;
+
 public class PageInfo {
 	public long size;
+
 	public PageInfo(String fn, long size) {
 		this.fn = fn;
-		this.size=size;
+		this.size = size;
 	}
 
 	public String fn;
 	public Page page;
+
+	public String toString() {
+		String s;
+		if (fn == null) {
+			s = "New Text...";
+		} else {
+			File f = new File(fn);
+			s = f.getName() + " " + f.getParent();
+		}
+		return s;
+	}
 
 }
