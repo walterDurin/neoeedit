efe289fe3987535b5439b23603c5915227b0d237
neoedmund <neoedmund@380faea4-4b26-11de-8d14-a1ddb34ee23c>
2010-03-09 09:00:31 +0000

    bugfix and upgrade

--- src/neoe/ne/Main.java
+++ src/neoe/ne/Main.java
@@ -1,13 +1,16 @@
 package neoe.ne;
 
+import java.io.File;
+
 public class Main {
 	public static void main(String[] args) throws Exception {
-		EditWindow editor = new EditWindow();
+		EditPanel editor = null;
 		if (args.length > 0) {
-			editor.openFile(args[0]);
+			editor=new EditPanel(new File(args[0]));
 		}else{
-			editor.newEmptyFile(null);
+			editor=new EditPanel("neoeedit");
+			editor.page.ptSelection.selectAll();
 		}
-		editor.show(true);
+		editor.openWindow();
 	}
 }
